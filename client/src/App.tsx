5a267a1b6e032624b922d559120f18a08f211417
Pol <polmonteroalegre@gmail.com>
2024-10-05 21:04:28 +0200

    Navbar template added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import Hero from "./components/Hero"
-
+import FloatingNav from "./components/ui/Navbar.tsx"
 function App() {
   return (
     <>
       <main className="relative bg-black flex justify-center items-center felx-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full ">
-          <Hero></Hero>
+          <FloatingNav navItems={[{ name: "pol", link: "#" }]} />
+          <Hero />
         </div>
       </main>
     </>
