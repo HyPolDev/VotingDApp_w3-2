ad7ca5f8977c4cb8d60da5100828d9f77b230e05
Pol <polmonteroalegre@gmail.com>
2024-12-04 11:07:22 +0100

    FEAT: error message on session not found

--- client/src/components/ui/SearchBar.tsx
+++ client/src/components/ui/SearchBar.tsx
@@ -7,6 +7,7 @@ import { addVotingData } from '../../store/votingSlice.ts'
 
 const SearchBar = () => {
     const [sessionId, setSessionId] = useState("")
+    const [errorState, setErrorState] = useState("")
     const data = useSelector((state: any) => state.voting)
     const dispatch = useDispatch();
     const saveVotingData = (sessionId: string, data: any) => {
@@ -18,31 +19,38 @@ const SearchBar = () => {
         let id = parseInt(sessionId)
         console.log("Looking for the session...")
 
-        const voteTimeLeft = Number(await getRemainingTime(id));  // Convert BigInt to Number
-        const voteResult = await getResults(id);
-        const voteStatus = await isVotingActive(id);
-        const votingTitle = await getTitle(id);
+        try {
+            const voteTimeLeft = Number(await getRemainingTime(id));  // Convert BigInt to Number
+            const voteResult = await getResults(id);
+            const voteStatus = await isVotingActive(id);
+            const votingTitle = await getTitle(id);
+            const VotingData = {
+                "TimeLeft": voteTimeLeft, // This is now a number, not BigInt
+                "Results": voteResult.map((result: any, index: any) => ({
+                    name: result.name,
+                    index: Number(index), // Convert index to number
+                    voteCount: result.voteCount.toString()  // Convert BigInt to string
+                })),
+                "Status": voteStatus,
+                "Title": votingTitle,
+                "id": sessionId
+            };
 
-        const VotingData = {
-            "TimeLeft": voteTimeLeft, // This is now a number, not BigInt
-            "Results": voteResult.map((result: any, index: any) => ({
-                name: result.name,
-                index: Number(index), // Convert index to number
-                voteCount: result.voteCount.toString()  // Convert BigInt to string
-            })),
-            "Status": voteStatus,
-            "Title": votingTitle,
-            "id": sessionId
-        };
+            console.log(data);
 
-        console.log(data);
+            saveVotingData(sessionId, VotingData)
+            inputRef.current.value = "";
+        } catch (error) {
+            setErrorState(`Could not find the session ${sessionId}`)
+            setTimeout(() => setErrorState(""), 2000)
+        }
 
-        saveVotingData(sessionId, VotingData)
-        inputRef.current.value = "";
     }
 
     return (
+
         <div className='relative flex justify-center items-center mx-32 border-4 border-gray-300 bg-black-100 rounded-full'>
+            <h4 className="absolute text-red-600">{errorState}</h4>
             <input ref={inputRef} onChange={(e) => setSessionId(e.target.value)} placeholder="Insert a Voting Address..." type="text" name="searchBar" id="searchBar" className="h-12 w-full bg-transparent px-10 focus:outline-none" />
             <div onClick={() => searchSession()}>
                 <Button className={'h-12 w-20 z-10 -mt-2 -mb-2'} icon={<FaMagnifyingGlass />} />
